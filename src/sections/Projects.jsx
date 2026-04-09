c343ad50448beebe3d3a270d9f6fbf4fdee04749
dhen-delacruz <dhenielldelacruz123@gmail.com>
2026-04-09 22:55:34 +0800

    updating project section

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -35,7 +35,7 @@ export default function Projects() {
         </motion.div>
 
         {/* Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {projects.map((project, i) => (
             <motion.div
               key={project.id}
